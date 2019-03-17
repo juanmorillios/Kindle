dc58bf0dfa9c6b12865bc38761664991fb3d3dbc
Juan Morillo <jmorillos@uoc.edu>
2019-03-17 17:17:16 +0100

    Add Model Book

---
+++ Kindle/Model/Book.swift
@@ -0,0 +1,20 @@
+//
+//  Book.swift
+//  Kindle
+//
+//  Created by Jmorillo on 17/03/2019.
+//  Copyright © 2019 MorilloApps. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    
+    let title: String
+    let author: String
+    
+    init(title: String, author: String) {
+        self.title = title
+        self.author = author
+    }
+}
