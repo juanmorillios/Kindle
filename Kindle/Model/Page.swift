f9112893934d2e33f97cc1698627c23f1c0fe6e5
Juan Morillo <jmorillos@uoc.edu>
2019-03-17 17:20:20 +0100

    Add Model Page.

---
+++ Kindle/Model/Page.swift
@@ -0,0 +1,20 @@
+//
+//  Page.swift
+//  Kindle
+//
+//  Created by Jmorillo on 17/03/2019.
+//  Copyright © 2019 MorilloApps. All rights reserved.
+//
+
+import Foundation
+
+class Page {
+    
+    let number: Int
+    let text: String
+    
+    init(number: Int, text: String) {
+        self.number = number
+        self.text = text
+    }
+}
