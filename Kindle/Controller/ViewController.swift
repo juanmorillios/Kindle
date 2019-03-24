e297ced05418523db5a94647a3021d2298dc3b52
Juan Morillo <jmorillos@uoc.edu>
2019-03-24 18:19:33 +0100

    Killed Storyboard and make UI with code.

--- Kindle/Controller/ViewController.swift
+++ Kindle/Controller/ViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var books: [Book]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Kindle"
+       self.view.backgroundColor = .blue
+        
+        // Load data
+        setupBooks()
     }
+    
+    // Sample Data
+    func setupBooks() {
+        
+        let page1 = Page(number: 1, text: "My first page")
+        let page2 = Page(number: 2, text: "My second page")
+        
+        let pages = [page1, page2]
+        
+        let book1 = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: pages)
+        let book2 = Book(title: "Bill Gates", author: "Michael Becraft", pages: pages)
+        
+        guard let books = self.books else { return }
+        print(books)
+        
+    }
+    
+    
 
 
 }
