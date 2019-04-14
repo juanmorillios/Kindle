096e2b37efbe0803667d879b304454b38d3df48a
Juan Morillo <jmorillos@uoc.edu>
2019-04-14 18:14:37 +0200

    Add UIViews to TableViewCell with code.

--- Kindle/View/BookCell.swift
+++ Kindle/View/BookCell.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class BookCell: UITableViewCell {
+    
+    let coverImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.frame = CGRect(x: 8, y: 8, width: 50, height: 64)
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 66, y: 20, width: UIScreen.main.bounds.width, height: 20)
+        label.text = "Name Book"
+        return label
+    }()
+    
+    let authorLabel: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 66, y: 48, width: 200, height: 20)
+        label.text = "Name Author"
+        return label
+    }()
+    
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         backgroundColor = .orange
+        self.addSubview(coverImageView)
+        self.addSubview(titleLabel)
+        self.addSubview(authorLabel)
     }
     
     required init?(coder aDecoder: NSCoder) {
